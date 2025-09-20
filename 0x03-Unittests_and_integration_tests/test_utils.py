0be281e02dd84cba04e54bad4339f9b8fb9aae88
Elite6802 <95652067+Elite6802@users.noreply.github.com>
2025-09-20 15:19:41 +0300

    Add files via upload

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,8 @@ Unit tests for the utils.access_nested_map function.
 """
 import unittest
 from parameterized import parameterized
-from fixtures import access_nested_map
+from unittest.mock import patch, Mock
+from fixtures import access_nested_map, get_json
 
 class TestAccessNestedMap(unittest.TestCase):
     """
@@ -32,3 +33,23 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as cm:
             access_nested_map(nested_map, path)
         self.assertEqual(str(cm.exception), f"'{expected_key}'")
+
+class TestGetJson(unittest.TestCase):
+    """
+    Tests for the get_json function.
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """
+        Tests that get_json returns the expected payload.
+        """
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+
+        with patch('requests.get', return_value=mock_response) as mock_get:
+            result = get_json(test_url)
+            mock_get.assert_called_once_with(test_url)
+            self.assertEqual(result, test_payload)
