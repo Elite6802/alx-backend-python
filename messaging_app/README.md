0bcc81423aa3f8b284a78d8800c3b517cdc952b7
Elite6802 <95652067+Elite6802@users.noreply.github.com>
2025-09-21 11:15:36 +0300

    Add files via upload

---
+++ messaging_app/README.md
@@ -0,0 +1,5 @@
+## LOGINS CREDENTIALS FOR SUPERUSER
+
+username: admin
+password: @Kelvinm2
+email: kelvinmutinda347@gmail.com
--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
-from rest_framework.routers import routers
+from rest_framework_nested.routers import NestedDefaultRouter
+from rest_framework.routers import DefaultRouter
 from .views import ConversationViewSet, MessageViewSet
 
 # Create a router and register our viewsets with it.
-router = routers.DefaultRouter()
+router = DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 # router.register(r'messages', MessageViewSet, basename='message')
 
-conversations_router = routers.NestedDefaultRouter(router, r'conversations', lookup='conversation')
+conversations_router = NestedDefaultRouter(router, r'conversations', lookup='conversation')
 conversations_router.register(r'messages', MessageViewSet, basename='conversation-messages')
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
